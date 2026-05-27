6c82db9f6e07f9df8dbe1feab552432d807c3488
David Berkompas <david.berkompas@gmail.com>
2026-05-27 08:58:39 -0500

    Update campaign.ts

--- src/data/campaign.ts
+++ src/data/campaign.ts
@@ -1,25 +1,130 @@
 export const campaignGroups: Record<string, string[]> = {
-  'Night of the Zealot': [
-    'The Gathering',
-    'The Midnight Masks',
-    'The Devourer Below',
-  ],
+    'Night of the Zealot': [
+        'The Gathering',
+        'The Midnight Masks',
+        'The Devourer Below',
+    ],
 
-  'The Circle Undone': [
-    'Uncovering the Past',
-    'Circle of Sin',
-    'The Weaver\'s Will',
-    'Point of No Return',
-    'Echoes of the Past',
-    'Before the Black Throne',
-  ],
+    'The Dunwich Legacy': [
+        'Extracurricular Activity',
+        'The House Always Wins',
+        'The Miskatonic Museum',
+        'The Essex County Express',
+        'Blood on the Altar',
+        'Undimensioned and Unseen',
+        'Where Doom Awaits',
+        'Lost in Time and Space',
+    ],
 
-  'The Forgotten Age': [
-    'Dim Carcosa',
-    'The Circle Undone',
-    'The Weaver\'s Will',
-    'Point of No Return',
-    'Echoes of the Past',
-    'Before the Black Throne',
-  ],
+    'The Path to Carcosa': [
+        'Curtain Call',
+        'The Last King',
+        'Echoes of the Past',
+        'The Unspeakable Oath',
+        'A Phantom of Truth',
+        'The Pallid Mask',
+        'Black Stars Rise',
+        'Dim Carcosa',
+    ],
+
+    'The Forgotten Age': [
+        'The Untamed Wilds',
+        'The Doom of Eztli',
+        'Threads of Fate',
+        'The Boundary Beyond',
+        'Heart of the Elders',
+        'The City of Archives',
+        'The Depths of Yoth',
+        'Shattered Aeons',
+    ],
+
+    'The Circle Undone': [
+        'Disappearance at the Twilight Estate (Prologue)',
+        'The Witching Hour',
+        'At Death\'s Doorstep',
+        'The Secret Name',
+        'The Wages of Sin',
+        'For the Greater Good',
+        'Union and Disillusion',
+        'In the Clutches of Chaos',
+        'Before the Black Throne',
+    ],
+
+    'The Dream-Eaters: Campaign A': [
+        'Beyond the Gates of Sleep',
+        'The Search for Kadath',
+        'Dark Side of the Moon',
+        'Where the Gods Dwell',
+    ],
+
+    'The Dream-Eaters: Campaign B': [
+        'Waking Nightmare',
+        'A Thousand Shapes of Horror',
+        'Point of No Return',
+        'Weaver of the Cosmos',
+    ],
+
+    'The Innsmouth Conspiracy': [
+        'The Pit of Despair',
+        'The Vanishing of Elina Harper',
+        'In Too Deep',
+        'Devil Reef',
+        'Horror in High Gear',
+        'A Light in the Fog',
+        'The Lair of Dagon',
+        'Into the Maelstrom',
+    ],
+
+    'Edge of the Earth': [
+        'Ice and Death',
+        'Fatal Mirage',
+        'To the Forbidden Peaks',
+        'City of the Elder Things',
+        'The Heart of Madness',
+    ],
+
+    'The Scarlet Keys': [
+        'Riddles and Rain',
+        'Dancing Mad',
+        'Dead Heat',
+        'Dealings in the Dark',
+        'Dogs of War',
+        'On Thin Ice',
+        'Sanguine Shadows',
+        'Shades of Suffering',
+        'Without a Trace',
+        'Congress of the Keys',
+    ],
+
+    'The Feast of Hemlock Vale': [
+        'Written in Rock, Act 1',
+        'Written in Rock, Act 2',
+        'Hemlock House',
+        'The Silent Heath',
+        'The Lost Sister',
+        'The Thing in the Depths',
+        'The Twisted Hollow',
+        'The Longest Night',
+        'Fate of the Vale',
+        'The Vale',
+    ],
+
+    'The Drowned City': [
+        'One Last Job',
+        'The Western Wall',
+        'The Drowned Quarter',
+        'The Apiary',
+        'The Grand Vault',
+        'Court of the Ancients',
+        'Obsidian Canyons',
+        'Sepulchre of the Sleeper',
+        'The Doom of Arkham I',
+        'The Doom of Arkham II',
+    ],
+
+    'Brethren of Ash': [
+        'Spreading Flames',
+        'Smoke and Mirrors',
+        'Queen of Ash',
+    ],
 }
